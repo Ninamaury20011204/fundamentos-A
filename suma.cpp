d558857ef9c5a738e8d05d32f627c2a7d799d867
Ninamaury20011204 <nina.tapuyo.anapa@utelvt.edu.ec>
2022-09-24 23:31:06 -0600

    mi trabajo suma

---
+++ suma.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float x,y,z;
+ cout<<"ingrese 2 numero x y :";
+ cin>>x>>y;
+ z=x+y;
+ cout<<"el resultado es : ";
+ cout<<z<<endl;
+
+ return 0;
+
+
+}
+
