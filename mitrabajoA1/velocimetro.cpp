af023d2d3dd0c6badd98244f5ec4a4102332e4cc
Ninamaury20011204 <nina.tapuyo.anapa@utelvt.edu.ec>
2022-09-25 00:11:25 -0600

    mi trabajo A1

---
+++ mitrabajoA1/velocimetro.cpp
@@ -0,0 +1,15 @@
+// creador: Nina tapuyo
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float a,b,c;
+	cout<<"Ingrese la distancia ( en km) b= "; cin>>b;
+	cout<<"ingrese el tiempo(en horas) c= "; cin>>c;
+	a=b/c;
+	cout<<"la velocidad calculada es  de a= " <<c<<endl;
+	return 0;
+
+
+}
