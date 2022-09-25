830f2430f6018ea75974aa8be1781a5e784efd72
Ninamaury20011204 <nina.tapuyo.anapa@utelvt.edu.ec>
2022-09-24 21:20:56 -0600

    mi primer proyecto

---
+++ calculadora.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float x,y,s,r,d,m;
+cout<<"ingrese el valor de x="; cin>>x;
+cout<<" ingrese el valor de y="; cin>>y;
+
+s=x+y;
+r=x-y;
+d=x/y;
+m=x*y;
+
+cout<<" el resultado de la suma "<<x<<"<< +<<" <<y<<" ="<<s<<endl;
+cout<<" el resultado de la resta "<<x<<" -" <<y<<" = "<<r<<endl;
+cout<<" el resultado de la division"<<x<<" / " <<y<<" ="<<d<<endl;
+cout<<" el resultado de la multiplucacion"<<x<<" * "<<y<<" ="<<m<<endl;
+
+
+return 0;
+
+
+
+
+
+}
